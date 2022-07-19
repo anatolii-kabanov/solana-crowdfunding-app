d1d8612ede816f386bcd636288229781db63e9a9
Anatolii Kabanov <4akabanov@gmail.com>
2022-07-20 01:01:18 +0300

    Copy command to copy idl to the fronted folder.

--- crowdfunding-program/tests/crowdfunding-program.ts
+++ crowdfunding-program/tests/crowdfunding-program.ts
@@ -1,5 +1,5 @@
 import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
+import { Program, utils } from '@project-serum/anchor';
 import { CrowdfundingProgram } from '../target/types/crowdfunding_program';
 import assert from 'assert';
 
@@ -12,30 +12,34 @@ describe('crowdfunding-program', async () => {
     const program = anchor.workspace
         .CrowdfundingProgram as Program<CrowdfundingProgram>;
 
-    const compaingAccount = anchor.web3.Keypair.generate();
-    const user1 = anchor.web3.Keypair.generate();
-
+    const [compaingAccount] = utils.publicKey.findProgramAddressSync(
+        [
+            utils.bytes.utf8.encode("compaing_demo"),
+            anchorProvider.wallet.publicKey.toBuffer(),
+        ],
+        program.programId
+    );
+    
     it('Should create compaing', async () => {
         /* Call the create function via RPC */
         
         await program.methods
             .create('test compaing', 'test description', new anchor.BN(5))
             .accounts({
-                compaing: compaingAccount.publicKey,
-                user: user1.publicKey,
+                compaing: compaingAccount,
+                user: anchorProvider.wallet.publicKey,
                 systemProgram: anchor.web3.SystemProgram.programId,
             })
-            .signers([user1])
             .rpc();
 
         const compaingAcc = await program.account.compaing.fetch(
-            compaingAccount.publicKey,
+            compaingAccount,
         );
         assert.equal(compaingAcc.name, 'test compaing');
         assert.equal(compaingAcc.description, 'test description');
         assert.ok(compaingAcc.targetAmount.eq(new anchor.BN(5)));
-        assert.equal(compaingAcc.owner, user1.publicKey);
-        assert.equal(compaingAcc.amountDonated, new anchor.BN(0));
+        assert.ok(compaingAcc.owner.equals(anchorProvider.wallet.publicKey));
+        assert.ok(compaingAcc.amountDonated.eq(new anchor.BN(0)));
     });
 });
 
